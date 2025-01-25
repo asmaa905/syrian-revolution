d4094a56220e4d09099f960e67ab33b9f9deb5f9
asmaa905 <asmaa_abdelnaser38@yahoo.com>
2025-01-25 22:55:11 +0200

    resolve problem in image path

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 export default {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // If using the app directory
-  },
+  // experimental: {
+  //   appDir: true, // If using the app directory
+  // },
   webpack(config) {
     config.infrastructureLogging = { debug: true };
     return config;
+  },
+  images: {
+    domains: ["syrianrevolution1.com"], // Add the domain here
   },
 };
