22d1bb5d5a0a5e62b85322cbf305e87a12375645
asmaa905 <asmaa_abdelnaser38@yahoo.com>
2025-01-25 21:52:57 +0200

    resolve-some-errors-in-deply

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 export default {
   reactStrictMode: true,
-  // experimental: {
-  //   appDir: true, // If using the app directory
-  // },
+  experimental: {
+    appDir: true, // If using the app directory
+  },
   webpack(config) {
     config.infrastructureLogging = { debug: true };
     return config;
